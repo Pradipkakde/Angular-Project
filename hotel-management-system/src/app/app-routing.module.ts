4d12386b0ab157fb34cb1b8e0c586ec632c8ee05
Pradipkakde <kakde.pradip1@gmail.com>
2024-02-26 12:06:58 +0530

    instlled angular material

--- hotel-management-system/src/app/app-routing.module.ts
+++ hotel-management-system/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from 'src/common/components/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",component:LandingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hotel-management-system/src/common/shared/shared.module.ts
+++ hotel-management-system/src/common/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignupComponent } from '../components/signup/signup.component';
+import { SigninComponent } from '../components/signin/signin.component';
+import { LandingComponent } from '../components/landing/landing.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent,
+    SigninComponent,
+    LandingComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SignupComponent,
+    SigninComponent,
+    LandingComponent
   ]
 })
 export class SharedModule { }
