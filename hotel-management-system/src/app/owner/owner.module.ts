b295289400d95145a1c5ef5361bc8dbaa215e0c9
Pradipkakde <kakde.pradip1@gmail.com>
2024-02-26 21:10:12 +0530

    done lazy loading routing  with owner,admin,user module

--- hotel-management-system/src/app/owner/owner.module.ts
+++ hotel-management-system/src/app/owner/owner.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OwnerRoutingModule } from './owner-routing.module';
+import { SharedModule } from 'src/common/shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    OwnerRoutingModule
+    OwnerRoutingModule,
+    SharedModule
   ]
 })
 export class OwnerModule { }
--- hotel-management-system/src/common/shared/shared.module.ts
+++ hotel-management-system/src/common/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SigninComponent } from '../common-component/signin/signin.component';
+import { SignupComponent } from '../common-component/signup/signup.component';
+import { LandingComponent } from '../common-component/landing/landing.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SigninComponent,
+    SignupComponent,
+    LandingComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SigninComponent,
+    SignupComponent,
+    LandingComponent
+
   ]
 })
 export class SharedModule { }
